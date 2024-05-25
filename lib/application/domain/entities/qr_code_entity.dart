683c8568cdd942003cc325d3f4c4c4a530d4b2f1
IhorDovh <holdargraf@gmail.com>
2024-05-25 11:48:49 +0300

    category on qr_code will be relised later

--- lib/application/domain/entities/qr_code_entity.dart
+++ lib/application/domain/entities/qr_code_entity.dart
@@ -4,7 +4,7 @@ import 'package:geolocator/geolocator.dart';
 class QrCodeEntity extends Equatable {
   final String name;
   final String url;
-  //final String category;
+  //final Category category;
   final Position position;
   final DateTime createdAt;
   final DateTime updatedAt;
