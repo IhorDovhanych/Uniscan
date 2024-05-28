7e5a2f6911f33145f1dbb193062064c03c32016d
IhorDovh <holdargraf@gmail.com>
2024-05-28 13:49:11 +0300

    update geo_location_service

--- lib/application/data/services/geo_location_service.dart
+++ lib/application/data/services/geo_location_service.dart
@@ -1,27 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:geolocator_apple/geolocator_apple.dart';
+import 'package:geolocator_android/geolocator_android.dart';
 
 abstract class GeoLocationService {
-  Future<Position> _determinePosition();
+  Future<void> getPermissions();
+  Future<Position> determinePosition();
+  Future<void> ensureLocationServiceEnabled();
+  Future<void> ensurePermissionsGranted();
+  Future<Stream<Position>> getPositionStream();
 }
 
 class GeoLocationServiceImpl implements GeoLocationService {
   GeoLocationServiceImpl();
 
   @override
-  Future<Position> _determinePosition() async {
-    await _ensureLocationServiceEnabled();
-    await _ensurePermissionsGranted();
-    return await Geolocator.getCurrentPosition();
+  Future<void> getPermissions() async {
+    await ensureLocationServiceEnabled();
+    await ensurePermissionsGranted();
   }
 
-  Future<void> _ensureLocationServiceEnabled() async {
+  @override
+  Future<Position> determinePosition() async =>
+      await Geolocator.getCurrentPosition();
+
+  @override
+  Future<void> ensureLocationServiceEnabled() async {
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       throw Future.error('Location services are disabled.');
     }
   }
 
-  Future<void> _ensurePermissionsGranted() async {
+  @override
+  Future<void> ensurePermissionsGranted() async {
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
@@ -33,5 +47,43 @@ class GeoLocationServiceImpl implements GeoLocationService {
       throw Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
     }
+  }
+
+  @override
+  Future<Stream<Position>> getPositionStream() async {
+    await ensureLocationServiceEnabled();
+    await ensurePermissionsGranted();
+
+    late LocationSettings locationSettings;
+
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      locationSettings = AndroidSettings(
+          accuracy: LocationAccuracy.high,
+          distanceFilter: 100,
+          forceLocationManager: true,
+          intervalDuration: const Duration(seconds: 10),
+          foregroundNotificationConfig: const ForegroundNotificationConfig(
+            notificationText:
+                "Example app will continue to receive your location even when you aren't using it",
+            notificationTitle: "Running in Background",
+            enableWakeLock: true,
+          ));
+    } else if (defaultTargetPlatform == TargetPlatform.iOS ||
+        defaultTargetPlatform == TargetPlatform.macOS) {
+      locationSettings = AppleSettings(
+        accuracy: LocationAccuracy.high,
+        activityType: ActivityType.fitness,
+        distanceFilter: 100,
+        pauseLocationUpdatesAutomatically: true,
+        showBackgroundLocationIndicator: false,
+      );
+    } else {
+      locationSettings = LocationSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 100,
+      );
+    }
+
+    return Geolocator.getPositionStream(locationSettings: locationSettings);
   }
 }
