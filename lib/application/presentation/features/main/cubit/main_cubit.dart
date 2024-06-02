b461d13e81e32cdf4d6702ff156efe50747edd02
IhorDovh <holdargraf@gmail.com>
2024-06-02 15:13:40 +0300

    separate main_cubit to cubit and state

--- lib/application/presentation/features/main/cubit/main_cubit.dart
+++ lib/application/presentation/features/main/cubit/main_cubit.dart
@@ -1,32 +1,13 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+part 'main_state.dart';
 class MainCubit extends Cubit<MainState> {
   MainCubit() : super(MainState());
 
   void setValue(final int value) => emit(state.copyWith(page: value));
 
-  void goToHomePage() {
-    emit(state.copyWith(page: 0));
-  }
+  void goToHomePage() => emit(state.copyWith(page: 0));
 
   void goToCameraPage() => emit(state.copyWith(page: 1));
-}
-
-class MainState extends Equatable {
-  const MainState({
-    this.page = 1,
-  });
-
-  final int page;
-
-  @override
-  List<Object?> get props => [page];
-
-  MainState copyWith({
-    final int? page,
-  }) =>
-      MainState(
-        page: page ?? this.page,
-      );
 }
